d42204187190292f8b44c36581d4613c35ac1b8a
sanjayaharshana <sanjayaharasana@gmail.com>
2024-12-18 00:05:47 +0530

    Centers Updates

---
+++ app/Filament/Resources/CentersResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CentersResource\Pages;
+use App\Filament\Resources\CentersResource\RelationManagers;
+use App\Models\Centers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CentersResource extends Resource
+{
+    protected static ?string $model = Centers::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCenters::route('/'),
+            'create' => Pages\CreateCenters::route('/create'),
+            'edit' => Pages\EditCenters::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,8 +5,18 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data):Model
+    {
+        // Custom logic for storing data
+        $data['password'] = bcrypt($data['password']); // Encrypt password manually
+
+        // Save to the database
+        return static::getModel()::create($data);
+    }
 }
